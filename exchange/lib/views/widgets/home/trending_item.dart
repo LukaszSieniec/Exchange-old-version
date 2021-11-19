ab7462831a433ae7a3482674268b90e6a8549663
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2021-11-19 04:43:20 +0100

    TrendingItem - add chart, trending icon and refactor

--- exchange/lib/views/widgets/home/trending_item.dart
+++ exchange/lib/views/widgets/home/trending_item.dart
@@ -1,6 +1,7 @@
 import 'package:exchange/constants/colors.dart';
 import 'package:exchange/utils/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class TrendingItem extends StatelessWidget {
   const TrendingItem({Key? key}) : super(key: key);
@@ -20,23 +21,76 @@ class TrendingItem extends StatelessWidget {
             color: const Color(MyColors.trendingBackground),
             child: Container(
                 margin: const EdgeInsets.all(8.0),
-                child: Column(children: [
-                  Row(children: [
-                    const Icon(Icons.attach_money_outlined,
-                        size: 48.0, color: Colors.white),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text('BTC',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24.0)),
-                          Text('Bitcoin',
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 20.0))
-                        ])
-                  ])
-                ]))));
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(children: [
+                              const Icon(Icons.attach_money_outlined,
+                                  size: 40.0, color: Colors.white),
+                              Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text('BTC',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20.0)),
+                                    Text('Bitcoin',
+                                        style: TextStyle(
+                                            color: Colors.grey, fontSize: 16.0))
+                                  ])
+                            ]),
+                            getIcon()
+                          ]),
+                      Container(
+                        margin: const EdgeInsets.all(12.0),
+                        child: SfSparkLineChart(
+                            color: Colors.green,
+                            data: exampleData,
+                            axisLineWidth: 0),
+                      ),
+                      const Text('\$ 78,43',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0))
+                    ]))));
   }
 }
+
+Icon getIcon() {
+  return exampleData[exampleData.length - 1] >
+          exampleData[exampleData.length - 2]
+      ? const Icon(Icons.trending_up_outlined, color: Colors.green, size: 32)
+      : const Icon(Icons.trending_down_outlined, color: Colors.red, size: 32);
+}
+
+const List<int> exampleData = [
+  10,
+  6,
+  8,
+  5,
+  11,
+  5,
+  2,
+  7,
+  3,
+  6,
+  8,
+  10,
+  10,
+  6,
+  8,
+  5,
+  11,
+  5,
+  2,
+  7,
+  3,
+  6,
+  8,
+  7
+];
