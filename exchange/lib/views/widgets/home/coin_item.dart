69d017f947ccc1d097ac04f3eb1b3dec0e459059
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2021-11-19 20:10:20 +0100

    CoinItem - Add new elements

--- exchange/lib/views/widgets/home/coin_item.dart
+++ exchange/lib/views/widgets/home/coin_item.dart
@@ -1,6 +1,7 @@
 import 'package:exchange/constants/colors.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:exchange/views/widgets/home/trending_item.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class CoinItem extends StatelessWidget {
   const CoinItem({Key? key}) : super(key: key);
@@ -15,22 +16,51 @@ class CoinItem extends StatelessWidget {
         ),
         elevation: 2,
         color: const Color(MyColors.coinBackground),
-        child: Padding(
+        child: Container(
             padding: const EdgeInsets.all(12.0),
             child: Row(children: [
-              const Icon(Icons.attach_money_outlined,
-                  size: 40.0, color: Colors.white),
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text('BTC',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0)),
-                    Text('Bitcoin',
-                        style: TextStyle(color: Colors.grey, fontSize: 16.0))
-                  ])
+              Expanded(
+                flex: 2,
+                child: Row(
+                  children: [
+                    const Icon(Icons.attach_money_outlined,
+                        size: 40.0, color: Colors.white),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text('BTC',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20.0)),
+                          Text('Bitcoin',
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 16.0))
+                        ]),
+                  ],
+                ),
+              ),
+              Expanded(
+                  flex: 1,
+                  child: SfSparkLineChart(
+                      color: Colors.green,
+                      data: exampleData,
+                      axisLineWidth: 0)),
+              Expanded(
+                  flex: 2,
+                  child: Column(children: [
+                    const Text('\$59,831,78',
+                        style: TextStyle(color: Colors.white, fontSize: 16.0)),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Icon(Icons.arrow_drop_up_outlined,
+                              size: 40.0, color: Colors.red),
+                          Text('8,02 %',
+                              style:
+                                  TextStyle(color: Colors.red, fontSize: 16.0))
+                        ])
+                  ]))
             ])));
   }
 }
