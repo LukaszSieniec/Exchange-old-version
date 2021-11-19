f92fdd454a656b24388db809e066fa358090f3a5
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2021-11-19 03:09:12 +0100

    TrendingItem - refactor

--- exchange/lib/views/widgets/home/trending_item.dart
+++ exchange/lib/views/widgets/home/trending_item.dart
@@ -10,26 +10,32 @@ class TrendingItem extends StatelessWidget {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Container(
-        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.all(8.0),
         width: SizeConfig.blockSizeHorizontal * 50.0,
         child: Card(
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(16.0)),
             elevation: 2,
             color: const Color(MyColors.trendingBackground),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Row(children: [
-                const Icon(Icons.attach_money_outlined),
-                Column(children: const [
-                  Text(
-                    'BTC',
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
-                  Text('Bitcoin', style: TextStyle(color: Colors.white))
-                ])
-              ])
-            ])));
+            child: Container(
+                margin: const EdgeInsets.all(8.0),
+                child: Column(children: [
+                  Row(children: [
+                    const Icon(Icons.attach_money_outlined,
+                        size: 48.0, color: Colors.white),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text('BTC',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24.0)),
+                          Text('Bitcoin',
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 20.0))
+                        ])
+                  ])
+                ]))));
   }
 }
