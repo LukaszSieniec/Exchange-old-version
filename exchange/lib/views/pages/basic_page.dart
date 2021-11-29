bf0feacf8cc4d5a3b707702d19701158934f8913
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2021-11-29 17:52:00 +0100

    BasicPage - refactor

--- exchange/lib/views/pages/basic_page.dart
+++ exchange/lib/views/pages/basic_page.dart
@@ -14,54 +14,59 @@ class BasicPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color(MyColors.background),
-        bottomNavigationBar: Container(
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(32.0),
-                    topLeft: Radius.circular(32.0)),
-                boxShadow: [
-                  BoxShadow(color: Colors.white, spreadRadius: 1, blurRadius: 4)
-                ]),
-            child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(32.0),
-                    topRight: Radius.circular(32.0)),
-                child: BottomNavigationBar(
-                    onTap: (index) =>
-                        BlocProvider.of<BottomNavigationBarBloc>(context).add(
-                            BottomNavigationBarUpdated(AppTab.values[index])),
-                    unselectedItemColor: Colors.grey[400],
-                    selectedItemColor: Colors.white,
-                    backgroundColor: const Color(MyColors.background),
-                    elevation: 8,
-                    items: const <BottomNavigationBarItem>[
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.home), label: MyLabels.home),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.attach_money),
-                          label: MyLabels.wallet),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.history),
-                          label: MyLabels.transactions)
-                    ]))),
-        body: SafeArea(
-            child: Container(
-                margin: const EdgeInsets.only(
-                    top: 16.0, left: 12.0, right: 12.0, bottom: 12.0),
-                child: BlocBuilder<BottomNavigationBarBloc, AppTab>(
-                    builder: (context, state) {
-                  switch (state) {
-                    case AppTab.home:
-                      return const HomePage();
-                    case AppTab.wallet:
-                      return const WalletPage();
-                    case AppTab.transactions:
-                      return const TransactionsPage();
-                    default:
-                      return Container();
-                  }
-                }))));
+    return BlocBuilder<BottomNavigationBarBloc, AppTab>(
+        builder: (context, state) {
+      return Scaffold(
+          backgroundColor: const Color(MyColors.background),
+          bottomNavigationBar: Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(32.0),
+                      topLeft: Radius.circular(32.0)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.white, spreadRadius: 1, blurRadius: 4)
+                  ]),
+              child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(32.0),
+                      topRight: Radius.circular(32.0)),
+                  child: BottomNavigationBar(
+                      currentIndex: AppTab.values.indexOf(state),
+                      onTap: (index) =>
+                          BlocProvider.of<BottomNavigationBarBloc>(context).add(
+                              BottomNavigationBarUpdated(AppTab.values[index])),
+                      unselectedItemColor: Colors.grey[400],
+                      selectedItemColor: Colors.white,
+                      backgroundColor: const Color(MyColors.background),
+                      elevation: 8,
+                      items: const <BottomNavigationBarItem>[
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.home), label: MyLabels.home),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.attach_money),
+                            label: MyLabels.wallet),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.history),
+                            label: MyLabels.transactions)
+                      ]))),
+          body: SafeArea(
+              child: Container(
+                  margin: const EdgeInsets.only(top: 16.0, left: 12.0, right: 12.0, bottom: 12.0),
+                  child: _buildBody(state))));
+    });
+  }
+
+  Widget _buildBody(AppTab state) {
+    switch (state) {
+      case AppTab.home:
+        return const HomePage();
+      case AppTab.wallet:
+        return const WalletPage();
+      case AppTab.transactions:
+        return const TransactionsPage();
+      default:
+        return Container();
+    }
   }
 }
