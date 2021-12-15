bd22abbba3c2acaed4cca9647776534ad3e8ef10
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2021-12-15 16:45:33 +0100

    Refactor BuyCryptocurrencyPage

--- exchange/lib/views/pages/buy_cryptocurrency_page.dart
+++ exchange/lib/views/pages/buy_cryptocurrency_page.dart
@@ -19,46 +19,41 @@ class BuyCryptocurrencyPage extends StatelessWidget {
                     alignment: AlignmentDirectional.centerEnd,
                     child: Container(
                         margin: const EdgeInsets.only(right: 8.0),
-                        child: const Text('\$ 378.12',
+                        child: const Text('Balance: 378.12',
                             style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 24.0,
                                 fontWeight: FontWeight.bold)))),
                 leading: IconButton(
                     icon: const Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    })),
+                    onPressed: () => Navigator.of(context).pop())),
             body: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const Divider(height: 2.0, color: Colors.white),
-                  Container(
-                      margin: const EdgeInsets.only(left: 24.0, right: 24.0),
-                      child: Column(children: [
-                        const Text('250 \$',
-                            style:
-                                TextStyle(fontSize: 48.0, color: Colors.white)),
-                        Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: const [
-                              Text('Estimated BTC',
-                                  style: TextStyle(
-                                      fontSize: 18.0, color: Colors.white)),
-                              Text('0,0871',
-                                  style: TextStyle(
-                                      fontSize: 18.0, color: Colors.white))
-                            ]),
-                        const Divider(height: 2.0, color: Colors.white)
-                      ])),
+                  const Text('250 \$',
+                      style: TextStyle(fontSize: 48.0, color: Colors.white)),
                   Container(
                       margin: const EdgeInsets.only(
                           bottom: 16.0, left: 8.0, right: 8.0),
-                      child: Column(children: const [
-                        Keyboard(),
-                        SizedBox(height: 32.0),
-                        ConfirmButton()
+                      child: Column(children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text('Estimated BTC: ',
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      color: Color(MyColors.colorText))),
+                              Text('0,0871',
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold))
+                            ]),
+                        const SizedBox(height: 16.0),
+                        const Keyboard(),
+                        const SizedBox(height: 32.0),
+                        const ConfirmButton()
                       ]))
                 ])));
   }
