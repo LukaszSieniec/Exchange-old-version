e1f68031e6bae16d538973f5bbf69311796eba94
Łukasz Sieniec <lukasz.sieniec@dataconsult.local>
2021-12-29 18:39:36 +0100

    Add SellCryptocurrencyPage and refactor Coins

---
+++ exchange/lib/views/pages/sell_cryptocurrency_page.dart
@@ -0,0 +1,59 @@
+import 'package:exchange/constants/my_constants.dart';
+import 'package:exchange/views/widgets/buy_cryptocurrency/confirm_button.dart';
+import 'package:exchange/views/widgets/buy_cryptocurrency/keyboard.dart';
+import 'package:flutter/material.dart';
+
+class SellCryptocurrencyPage extends StatelessWidget {
+  const SellCryptocurrencyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(MyColors.background),
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            flexibleSpace: SafeArea(
+                child: Align(
+                    alignment: AlignmentDirectional.centerEnd,
+                    child: Container(
+                        margin: const EdgeInsets.only(right: 8.0),
+                        child: const Text('Balance: 1000',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold))))),
+            leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.of(context).pop())),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Divider(height: 2.0, color: Colors.white),
+              const Text('0',
+                  style: TextStyle(fontSize: 48.0, color: Colors.white)),
+              Container(
+                  margin: const EdgeInsets.only(
+                      bottom: 16.0, left: 8.0, right: 8.0),
+                  child: Column(children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text('Estimated \$: ',
+                              style: TextStyle(
+                                  fontSize: 16.0,
+                                  color: Color(MyColors.colorText))),
+                          Text('200',
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold))
+                        ]),
+                    const SizedBox(height: 16.0),
+                    const Keyboard(),
+                    const SizedBox(height: 32.0),
+                    const ConfirmButton()
+                  ]))
+            ]));
+  }
+}
--- exchange/lib/views/widgets/wallet/coins.dart
+++ exchange/lib/views/widgets/wallet/coins.dart
@@ -1,5 +1,6 @@
 import 'package:exchange/constants/my_constants.dart';
 import 'package:exchange/models/cryptocurrency.dart';
+import 'package:exchange/views/pages/sell_cryptocurrency_page.dart';
 import 'package:flutter/material.dart';
 
 class Coins extends StatelessWidget {
@@ -14,48 +15,51 @@ class Coins extends StatelessWidget {
             scrollDirection: Axis.vertical,
             itemCount: cryptocurrencies.length,
             itemBuilder: (BuildContext context, int index) {
-              return Card(
-                  elevation: 4,
-                  color: const Color(MyColors.brighterBackground),
-                  child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  CircleAvatar(
-                                    radius: 16.0,
-                                    backgroundImage: NetworkImage(
-                                        cryptocurrencies[index].image),
-                                    backgroundColor: Colors.transparent,
-                                  ),
-                                  const SizedBox(width: 8.0),
-                                  Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                            cryptocurrencies[index]
-                                                .symbol
-                                                .toUpperCase(),
-                                            style: const TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 20.0)),
-                                        Text(cryptocurrencies[index].name,
-                                            style: const TextStyle(
-                                                color:
-                                                    Color(MyColors.colorText),
-                                                fontSize: 16.0))
-                                      ])
-                                ]),
-                            Text('${cryptocurrencies[index].amount}',
-                                style: const TextStyle(
-                                    color: Colors.white, fontSize: 20.0))
-                          ])));
+              return GestureDetector(
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const SellCryptocurrencyPage())),
+                  child: Card(
+                      elevation: 4,
+                      color: const Color(MyColors.brighterBackground),
+                      child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      CircleAvatar(
+                                        radius: 16.0,
+                                        backgroundImage: NetworkImage(
+                                            cryptocurrencies[index].image),
+                                        backgroundColor: Colors.transparent,
+                                      ),
+                                      const SizedBox(width: 8.0),
+                                      Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                                cryptocurrencies[index]
+                                                    .symbol
+                                                    .toUpperCase(),
+                                                style: const TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 20.0)),
+                                            Text(cryptocurrencies[index].name,
+                                                style: const TextStyle(
+                                                    color: Color(
+                                                        MyColors.colorText),
+                                                    fontSize: 16.0))
+                                          ])
+                                    ]),
+                                Text('${cryptocurrencies[index].amount}',
+                                    style: const TextStyle(
+                                        color: Colors.white, fontSize: 20.0))
+                              ]))));
             }));
   }
 }
